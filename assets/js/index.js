a2ca4ba3c38042dac13b15c05126c57d06e6bda8
TSYF <tsyf1999@gmail.com>
2023-05-29 23:25:31 -0400

    bugfix usuario no logueado muestra boton log out
    Se modifica el uso de currentUser en index para que actualice los datos
    de usuario en caso de estar logueado y que los borre en caso de no
    estarlo.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,12 +5,22 @@ import userUtils from './utils/user.js';
 $(() => {
 	userUtils.currentUser()
         .then(res => {
-            state.user.loadUser();
-            $("#displayUser").text("@" + res.email.split("@")[0]);
-            $("#displayUserManagement").text("@" + res.email.split("@")[0]);
-            $("#sign_in").addClass("d-none");
-            $("#sign_up").addClass("d-none");
-            $("[data-signout]").removeClass("d-none");
+            if (res) {
+                state.user.loadUser();
+                $("#displayUser").text("@" + res.email.split("@")[0]);
+                $("#displayUserManagement").text("@" + res.email.split("@")[0]);
+                
+                $("#sign_in").addClass("d-none");
+                $("#sign_up").addClass("d-none");
+                $("[data-signout]").removeClass("d-none");
+                return
+            }
+
+            $("#displayUser").text("");
+            $("#displayUserManagement").text("");
+            $("#sign_in").removeClass("d-none");
+            $("#sign_up").removeClass("d-none");
+            $("[data-signout]").addClass("d-none");
         });
 
     $("[data-toggle-user]").click(e => {
