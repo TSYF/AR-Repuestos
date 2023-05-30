bbb62734a622967f870af75bc27fd0550cb6e231
TSYF <tsyf1999@gmail.com>
2023-05-29 22:49:22 -0400

    Creado código en JS principal para la sesión de usuario
    Se añadió código para asegurar la persistencia de la información del
    usuario en la interfaz y el JS.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,33 @@
 import state from "./utils/state.js";
 import userUtils from './utils/user.js';
 
-parent.userUtils = userUtils;
-parent.state = state;
+
+$(() => {
+	userUtils.currentUser()
+        .then(res => {
+            state.user.loadUser();
+            $("#displayUser").text("@" + res.email.split("@")[0]);
+            $("#displayUserManagement").text("@" + res.email.split("@")[0]);
+            $("#sign_in").addClass("d-none");
+            $("#sign_up").addClass("d-none");
+            $("[data-signout]").removeClass("d-none");
+        });
+
+    $("[data-toggle-user]").click(e => {
+        $("#manageUser").toggleClass("d-none");
+    });
+
+    $("[data-signout]").click(e => {
+        userUtils.userSignOut()
+            .then(res => {
+                $("#displayUser").text("");
+                $("#displayUserManagement").text("");
+                $("#sign_in").removeClass("d-none");
+                $("#sign_up").removeClass("d-none");
+                $("[data-signout]").addClass("d-none");
+            });       
+            
+    });
+
+    
+});
