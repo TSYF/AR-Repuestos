d4b42b893c97e7b3ccc3cb689c28a5a34eff74ed
TSYF <tsyf1999@gmail.com>
2023-06-29 00:59:58 -0400

    Modelos registrados en admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Categoria)
+admin.site.register(models.Producto)
+admin.site.register(models.EstadoProducto)
+admin.site.register(models.Servicio)
+admin.site.register(models.Cliente)
+admin.site.register(models.ContactoServicio)
