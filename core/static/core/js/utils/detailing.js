707c9911087971929511e4b21ab902354d10f6fa
TSYF <tsyf1999@gmail.com>
2023-06-29 01:07:18 -0400

    Arreglado JS detailing. Validación DV estaba mal

--- core/static/core/js/utils/detailing.js
+++ core/static/core/js/utils/detailing.js
@@ -6,6 +6,19 @@ const dvEsperadoMap = {
     11: 0
 };
 
+function dgv(rut)    //digito verificador
+{  
+      let M = 0;
+      let S = 1;
+
+	  for(; rut ;rut = Math.floor(rut / 10)) {
+        S = ( S + rut % 10 * ( 9 - M++ % 6) ) % 11;
+      }
+	  //return S?S-1:'k';
+      
+      return (S ? S - 1 : 'k');
+ }
+
 export const validaEmail = email => {
     return emailRegex.test(email);
 };
@@ -15,19 +28,23 @@ export const validaRut = rut => {
     rut = rut.replace(/[.-]/g, '');
   
     if (!rutRegex.test(rut)) {
-        return false; 
+        return false;
     }
     const rutSinDigito = rut.slice(0, -1);
     const rutSinDigitoArr = rutSinDigito.split("");
     const digitoVerificador = rut.slice(-1).toUpperCase();
+    const rutSinDigitoArrInverso = rutSinDigitoArr.reverse();
     
-    const suma = rutSinDigitoArr.reduce((acumulador, actual, i) => {
+    const suma = rutSinDigitoArrInverso.reduce((acumulador, actual, i) => {
+
         const multiplo = (i % 6) + 2;
+
         return acumulador + actual * multiplo;
       }, 0);
+
     let dvEsperado = 11 - (suma % 11);
 
-    dvEsperado = dvEsperadoMap[dvEsperado] || dvEsperado.toString();
+    dvEsperado = dvEsperadoMap[dvEsperado] || dvEsperado;
 
     return digitoVerificador === dvEsperado;
 }
