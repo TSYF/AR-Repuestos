ef8e62cddb1a49bcf81bb5150207af2f6fb2ea34
TSYF <tsyf1999@gmail.com>
2023-06-29 01:09:01 -0400

    Mejorado js view detailing. Incluye funcionalidad

--- core/static/core/js/views/detailing.js
+++ core/static/core/js/views/detailing.js
@@ -2,16 +2,22 @@ import { validaEmail, validaRut, validaTelefono } from '../utils/detailing.js';
 
 const form = document.getElementById("form_contacto");
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", async e => {
     e.preventDefault();
 
+    const csrf = document.querySelector('[name="csrfmiddlewaretoken"]');
+    const nombreInput = document.getElementById("nombre");
     const rutInput = document.getElementById("rut");
     const emailInput = document.getElementById("email");
     const telefonoInput = document.getElementById("telefono"); 
+    const servicioInput = document.getElementById("servicio"); 
 
+    const csrfmiddlewaretoken = csrf.value;
     const rut = rutInput.value;
+    const nombre = nombreInput.value;
     const email = emailInput.value;
     const telefono = telefonoInput.value;
+    const servicio = servicioInput.value;
 
     if (!validaRut(rut)) {
         return alert("El RUT no es válido");
@@ -25,5 +31,28 @@ form.addEventListener("submit", e => {
         return alert("El número de Teléfono no es válido")
     }
 
-    return alert("Muchas gracias! Lo contactaremos a la brevedad");
+    // return alert("Muchas gracias! Lo contactaremos a la brevedad");
+
+    const data = {
+        rut,
+        nombre,
+        email,
+        telefono,
+        servicio
+    };
+
+    try {
+        const res = await fetch("/contacto-detailing/store", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRFToken": csrfmiddlewaretoken
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json());
+
+        console.log(res);
+    } catch (error) {
+        console.error(error);
+    }    
 });
