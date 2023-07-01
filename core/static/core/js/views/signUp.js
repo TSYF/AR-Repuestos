a4e4ce18fc95571128ebe28f9c560e176d443753
TSYF <tsyf1999@gmail.com>
2023-06-30 23:26:03 -0400

    Se arreglan direcciones de assets de sign in y sign up, se arregla js roto

--- core/static/core/js/views/signUp.js
+++ core/static/core/js/views/signUp.js
@@ -31,7 +31,7 @@ form.addEventListener("submit", e => {
                     $("#password").val("");
                     $("#confirm").val("");
                     $("#displayUser").text("@" + signedUser.email.split("@")[0]);
-                    open("index.html", "_self");
+                    open("", "_self");
                 }).catch(error => {
                     error.code && alert(userUtils.state.errorMessages[error.code]);
                 });
