7727b688c4141459909b92691cfe5955832e2539
TSYF <tsyf1999@gmail.com>
2023-07-02 22:07:21 -0400

    Funcionalidades carrito app core

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Producto, ContactoServicio, Cliente, Servicio
+from .forms import ProductoForm
 
 # Create your views here.
 
@@ -23,12 +24,50 @@ def tienda(request, page = 1):
 def producto(request, id):
 
     producto = Producto.objects.get(id=id)
+    productoForm = ProductoForm(producto.__dict__)
 
     data = {
-        "producto": producto
+        "producto": producto,
+        "form": productoForm
     }
-    
+    match request.method:
+        case 'GET':
+            if not request.session.get("carro", False):
+                request.session["carro"] = []
+            
+            pass            
+        
+        case 'POST':
+            carro = request.session["carro"]
+            
+            producto = {
+                "id": int(request.POST["id"]),
+                "nombre": request.POST["nombre"],
+                "precio": int(request.POST["precio"]),
+                "categoria": request.POST["categoria"],
+                "estado": request.POST["estado"],
+                "cantidad": 1,
+                "subtotal": int(request.POST["precio"]),
+            }
+            
+
+            item_in_carro_index = None
+
+            for i, item in enumerate(carro):
+                if item["id"] == producto["id"]:
+                    item_in_carro_index = i
+            
+            if item_in_carro_index != None:
+                carro[i]["cantidad"] += 1
+                carro[i]["subtotal"] += carro[i]["precio"]
+            else:
+                carro.append(producto)
+
+            request.session["carro"] = carro
+
     return render(request, "producto.html", data)
+    
+    
 
 def detailing_create(request):
     data = {
@@ -49,3 +88,19 @@ def sign_in(request):
 
 def sign_up(request):
     return render(request, "signUp.html")
+
+
+def carro_show(request):
+
+    carro = request.session["carro"]
+
+    total = 0
+
+    for item in carro:
+        total += item["subtotal"]
+
+    data = {
+        "carro": carro,
+        "total": total
+    }
+    return render(request, "carro.html", data)
